bc691ef51d628ab3e2e3ac5d0c59a1968a6a9208
jbethanc <jbethanc@gmail.com>
2023-10-29 16:10:15 -0500

    run format script

--- src/components/Step.svelte
+++ src/components/Step.svelte
@@ -1,17 +1,24 @@
 <script>
-     export let step;
+	export let step;
 </script>
 
-<a href="{step.href}" target="_blank" class="p-4 sm:p-6 md:p-8 flex flex-col gap-4 rounded-lg border border-solid border-violet-700 text-center group cursor-pointer hover:border-violet-400 duration-200">
-    <div class="bg-slate-950 grid place-items-center px-4 text-5xl md:text-6xl -mt-10 sm:-mt-12 md:-mt-14 lg:-mt-16 mx-atuo duration-200">
-        <i class={step.icon} />
-    </div>
-    <h3 class="font-medium text-xl sm:text-2xl md:text-3xl">{step.name}</h3>
-    <slot />
-    <div class="flex-1 flex justify-between gap-4 items-end">
-        <div class="ml-auto cursor-pointer duration-200 relative after:absolute after:top-0 after:h-0 after:right-full after:h-full after:bg-violet-700 after:w-full after:duration-200 hover:after:translate-x-full after:z-[-1] overflow-hidden">
-            <p class="relative z-4">Go to &rarr;</p>
-        </div>
-    </div>
-
+<a
+	href={step.href}
+	target="_blank"
+	class="p-4 sm:p-6 md:p-8 flex flex-col gap-4 rounded-lg border border-solid border-violet-700 text-center group cursor-pointer hover:border-violet-400 duration-200"
+>
+	<div
+		class="bg-slate-950 grid place-items-center px-4 text-5xl md:text-6xl -mt-10 sm:-mt-12 md:-mt-14 lg:-mt-16 mx-atuo duration-200"
+	>
+		<i class={step.icon} />
+	</div>
+	<h3 class="font-medium text-xl sm:text-2xl md:text-3xl">{step.name}</h3>
+	<slot />
+	<div class="flex-1 flex justify-between gap-4 items-end">
+		<div
+			class="ml-auto cursor-pointer duration-200 relative after:absolute after:top-0 after:h-0 after:right-full after:h-full after:bg-violet-700 after:w-full after:duration-200 hover:after:translate-x-full after:z-[-1] overflow-hidden"
+		>
+			<p class="relative z-4">Go to &rarr;</p>
+		</div>
+	</div>
 </a>
