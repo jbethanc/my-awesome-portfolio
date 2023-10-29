e6d818b70b8d08ce2f6d3a9e40521f312f8c3545
jbethanc <jbethanc@gmail.com>
2023-10-29 16:03:31 -0500

    build out footer

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -25,7 +25,7 @@
             <p class="text-base sm:text-lg md:text-xl">
                 My <span class="text-violet-400">favorite tech</span> includes JS, SvelteKit, Tailwind (other....)!
             </p>
-            <a href="https://www.linkedin.com/in/jbethancourt/" target="_blank" class="poppins blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
+            <a href="https://www.linkedin.com/in/jbethancourt/" target="_blank" class="poppins blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
                 <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200" />
                 <h4 class="relative z-9">Get in touch &rarr;</h4>
             </a>
@@ -134,5 +134,6 @@
         <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
             <p>Scroll to see more &rarr;</p>
         </div>
+        <p class="mx-auto">So why not invest?</p>
     </section>
 </main>
