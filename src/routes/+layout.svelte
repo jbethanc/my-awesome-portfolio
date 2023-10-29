2a9e15a46d1d8191990c6fa855d25650572f0aa8
jbethanc <jbethanc@gmail.com>
2023-10-29 15:46:09 -0500

    finish main content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,15 +12,17 @@
   }
 
 </script>
-
-<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+<!-- TODO: make goTop button not reliant on screen size? Seems odd to deprive it from desktop users if it already exists for smaller screens -->
+<div class="container relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
   
-  <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10 " + (
-    y > 0 ? 'opacity-full pointer-events-auto' : 'pointer-events-none opacity-0'
+  <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " + (
+    y > 0 ? " opacity-full pointer-events-auto" : " pointer-events-none opacity-0"
   )}>
-    <button on:click={goTop} class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer">
-      <i class="fa-solid fa-arrow-up grid place-items-center aspect-square" />
-    </button>
+      <button
+        on:click={goTop} 
+        class="ml-auto rounded-full aspect-square bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer grid place-items-center">
+          <i class="fa-solid fa-arrow-up" />
+      </button>
   </div>
   <Header y={y}/>
   <slot />
