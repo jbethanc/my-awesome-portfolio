0f2e4aa3e356fb2cd1e2de51f6d069bc8d7affbb
jbethanc <jbethanc@gmail.com>
2023-10-29 13:43:27 -0500

    complete header layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>Visit my <a href="https://www.linkedin.com/in/jbethancourt/">LinkedIn</a></p>
+<script>
+    import Main from '../components/Main.svelte'
+</script>
+
+<Main />
