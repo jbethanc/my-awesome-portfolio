3f6aef5d2326d15eeba7d7a89a8fc63190030179
jbethanc <jbethanc@gmail.com>
2023-10-29 13:05:29 -0500

    add particle bg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1 class="test-3xl font-bold underline text-green-400">Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit my <a href="https://www.linkedin.com/in/jbethancourt/">LinkedIn</a></p>
